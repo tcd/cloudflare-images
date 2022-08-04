a1dc6d9bd42d4ffe0b8ad2e835841d2f6e3edc84
Clay Dunston <claydunston@gmail.com>
2022-08-04 07:36:07 -0500

    Add `is-blank` helper method

---
+++ src/lib/is-blank.ts
@@ -0,0 +1,46 @@
+/**
+ * Returns `true` if the passed value is:
+ *
+ * - `undefined`
+ * - `null`
+ * - `NaN`
+ * - `""` (an empty string)
+ * - `[]` (an empty array)
+ * - `{}` (an empty object)
+ *
+ * @param data any value
+ */
+ export const isBlank = (data: any): boolean => {
+
+    // Null or undefined
+    if (data == undefined) { return true }
+    if (data == null)      { return true }
+
+    // NaN
+    if (data?.toString() == "NaN") {
+        return true
+    }
+
+    // Empty string
+    if (data === "") {
+        return true
+    }
+
+    // Empty array
+    if (Array.isArray(data) && data.length === 0) {
+        return true
+    }
+
+    // Empty object
+    // if (JSON.stringify(data) === "{}") {
+    //     return true;
+    // }
+
+    // Empty object
+    if (data.constructor === Object && Object.keys(data).length === 0) {
+        return true
+    }
+
+    // Looks like there is a value
+    return false
+}
